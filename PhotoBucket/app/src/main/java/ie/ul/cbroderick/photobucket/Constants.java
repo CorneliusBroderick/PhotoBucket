7a1fb9b4f2f9e12b954ed122532f60c76121dc91
CorneliusBroderick <9119124@studentmail.ul.ie>
2018-11-12 13:42:38 +0000

    changed collection path to photobucket

--- PhotoBucket/app/src/main/java/ie/ul/cbroderick/photobucket/Constants.java
+++ PhotoBucket/app/src/main/java/ie/ul/cbroderick/photobucket/Constants.java
@@ -3,7 +3,7 @@ package ie.ul.cbroderick.photobucket;
 public class Constants {
 
     public static final String TAG = "MQ";
-    public static final String COLLECTION_PATH = "moviequotes";
+    public static final String COLLECTION_PATH = "photobucket";
     public static final String KEY_QUOTE = "quote";
     public static final String KEY_MOVIE = "movie";
     public static final String KEY_CREATED = "created";
